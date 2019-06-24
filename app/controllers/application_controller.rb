66adcaeb6f4d725c712ce60b920c5a8e1a7575d7
Mickae Truman <mtruman@nyit.edu>
2019-06-24 20:43:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,8 @@ class ApplicationController < Sinatra::Base
   
  post '/songs' do
   erb :"/songs/songs"
- @song = params[:song]["title"]
- @song = params[:song]["length"]
+ @song = params[:song]["title"];
+ @song = params[:song]["length"];
  @song = params[:song]["play_count"]
  end
 
